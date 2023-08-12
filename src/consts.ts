f2892424a78ff6d9154213971000ac6b0fa1ba97
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-08-12 10:27:46 +0100

    Fix sitemap vars. Add title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE =
+  "Tiago Rodrigues - Student, Developer & Productivity Nerd";
+export const SITE_DESCRIPTION =
+  "I am a University Student that will one day become a Software Engineer. I am also a Productivity Nerd that loves to learn and share what I learn. Here you can find my blog, my projects and my resume.";
