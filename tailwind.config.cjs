e5a1e50716d67a01355a6d820954aa854717f311
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-08-12 10:29:22 +0100

    Configure base layout and styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#ff6464",
+				dark: "#21243d",
+				light: "#8695A4",
+
+			}
+			,
+			fontFamily: {
+				'sans': ['Roboto', ...defaultTheme.fontFamily.sans],
+				'heading': ['Playfair Display', ...defaultTheme.fontFamily.serif],
+			},
+		},
 	},
 	plugins: [],
 }
