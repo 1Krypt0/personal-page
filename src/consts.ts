5e2760f1f660a045790bdf1ad47884f2f27c5730
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-08-17 15:01:16 +0100

    Remove unneeded features

--- src/consts.ts
+++ src/consts.ts
@@ -5,3 +5,5 @@ export const SITE_TITLE =
   "Tiago Rodrigues - Student, Developer & Productivity Nerd";
 export const SITE_DESCRIPTION =
   "I am a University Student that will one day become a Software Engineer. I am also a Productivity Nerd that loves to learn and share what I learn. Here you can find my blog, my projects and my resume.";
+export const HERO_TITLE = "Hi, I'm Tiago, Software Developer.";
+export const HERO_TEXT = "I am a University Student from a small place called Milhazes living in Porto, Portugal. I love going to the gym, watching Benfica win and improving myself in every way I can, every single day. Welcome to my page!";
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,19 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function get(context) {
 	const posts = await getCollection('blog');
+	const projects = await getCollection('projects');
+	const allPosts = posts.map((post) => ({
+		...post.data,
+		link: `/blog/${post.slug}/`,
+	}));
+	const allProjects = projects.map((project) => ({
+		...project.data,
+		link: `/projects/${project.slug}/`,
+	}));
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
+		items: [...allPosts, ...allProjects],
 	});
 }
