8bdefeb06be54c079b30cf6c3c7ac66935073bed
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-10-29 14:15:13 +0000

    Add vercel analytics to blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tiagopbrodrigues.com',
-  integrations: [sitemap(), tailwind()]
+  site: "https://tiagopbrodrigues.com",
+  integrations: [sitemap(), tailwind()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
