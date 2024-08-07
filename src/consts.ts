d0e1f7cee593c7a99e67c508170db6a793f466d4
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2024-08-07 19:49:27 +0100

    Update hero title

--- src/consts.ts
+++ src/consts.ts
@@ -5,7 +5,7 @@ export const SITE_TITLE =
   "Tiago Rodrigues - Student, Developer & Productivity Nerd";
 export const SITE_DESCRIPTION =
   "I am a Master's graduate that will one day become an ML or Software Engineer. I am also a Productivity Nerd that loves to learn and share what I learn. Here you can find my blog, my projects and my resume.";
-export const HERO_TITLE = "Hi, I'm Tiago, Welcome to my world!.";
+export const HERO_TITLE = "Hi there, I'm Tiago. Welcome to my online world!";
 export const HERO_TEXT =
   "I am a Master's graduate hailing from a small place called Milhazes, now living in Porto, Portugal. I love going to the gym, watching Benfica win and improving myself in every way I can, every single day.";
 
