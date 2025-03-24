e287ae4b18dd861abea0915a3a0028df902e3d06
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2025-03-24 11:19:43 +0000

    Update tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,15 @@ import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://tiagopbrodrigues.com",
   integrations: [sitemap(), tailwind()],
   output: "static",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
