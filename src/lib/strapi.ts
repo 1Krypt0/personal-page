6af6861df88ae7cbe9ed516b7e354ddef43fd470
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2025-03-24 12:41:28 +0000

    Import projects from Strapi

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -42,18 +42,62 @@ const articleSchema = z.object({
   category: categorySchema,
 });
 
+const projectShowcaseSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  slug: z.string(),
+  content: z.string(),
+  projectLink: z.string().url(),
+  createdAt: z
+    .string()
+    .datetime()
+    .transform((str) => new Date(str)),
+  updatedAt: z
+    .string()
+    .datetime()
+    .transform((str) => new Date(str)),
+  publishedAt: z
+    .string()
+    .datetime()
+    .transform((str) => new Date(str)),
+  cover: z
+    .object({
+      url: z.string(),
+      alternativeText: z.string(),
+    })
+    .nullable(),
+  category: categorySchema,
+});
+
 export type Category = z.infer<typeof categorySchema>;
 export type Article = z.infer<typeof articleSchema>;
-//
-// export const getAllCategories = async (): Promise<Category[]> => {
-//   const path = "/api/categories";
-//   const url = new URL(path, STRAPI_URL);
-//
-//   const categoriesData = await fetch(url.href);
-//   const { data } = await categoriesData.json();
-//
-//   return data.map((item: any) => categorySchema.parse(item));
-// };
+export type ProjectShowcase = z.infer<typeof projectShowcaseSchema>;
+
+export const getAllProjects = async (): Promise<ProjectShowcase[]> => {
+  const path = "/api/project-showcases";
+  const url = new URL(path, STRAPI_URL);
+
+  // Build query parameters using qs to populate cover image data
+  url.search = qs.stringify({
+    populate: {
+      cover: {
+        fields: ["url", "alternativeText"],
+      },
+      category: {
+        fields: ["name", "slug"],
+      },
+    },
+  });
+
+  const articlesData = await fetch(url.href);
+
+  const { data } = await articlesData.json();
+
+  console.log("Data is");
+  console.log(data);
+
+  return data.map((item: any) => projectShowcaseSchema.parse(item));
+};
 
 export const getAllPosts = async (): Promise<Article[]> => {
   const path = "/api/articles";
@@ -77,138 +121,6 @@ export const getAllPosts = async (): Promise<Article[]> => {
 
   return data.map((item: any) => articleSchema.parse(item));
 };
-//
-// export const getPostsByCategory = async (
-//   category: string,
-// ): Promise<Article[]> => {
-//   const path = "/api/articles";
-//   const url = new URL(path, STRAPI_URL);
-//
-//   // Build query parameters using qs to populate cover image data
-//   url.search = qs.stringify({
-//     filters: {
-//       category: {
-//         name: {
-//           $eq: category,
-//         },
-//       },
-//     },
-//     populate: {
-//       cover: {
-//         fields: ["url", "alternativeText"],
-//       },
-//       author: {
-//         fields: ["name", "email"],
-//         populate: {
-//           avatar: {
-//             fields: ["url", "alternativeText"],
-//           },
-//         },
-//       },
-//       category: {
-//         fields: ["name", "slug"],
-//       },
-//       tags: {
-//         fields: ["name", "slug"],
-//       },
-//     },
-//   });
-//
-//   const articlesData = await fetch(url.href);
-//   const { data } = await articlesData.json();
-//
-//   return data.map((item: any) => articleSchema.parse(item));
-// };
-//
-// export const getPostsByTag = async (tag: string): Promise<Article[]> => {
-//   const path = "/api/articles";
-//   const url = new URL(path, STRAPI_URL);
-//
-//   // Build query parameters using qs to populate cover image data
-//   url.search = qs.stringify({
-//     filters: {
-//       tags: {
-//         name: {
-//           $eq: tag,
-//         },
-//       },
-//     },
-//     populate: {
-//       cover: {
-//         fields: ["url", "alternativeText"],
-//       },
-//       author: {
-//         fields: ["name", "email"],
-//         populate: {
-//           avatar: {
-//             fields: ["url", "alternativeText"],
-//           },
-//         },
-//       },
-//       category: {
-//         fields: ["name", "slug"],
-//       },
-//       tags: {
-//         fields: ["name", "slug"],
-//       },
-//     },
-//   });
-//
-//   const articlesData = await fetch(url.href);
-//   const { data } = await articlesData.json();
-//
-//   return data.map((item: any) => articleSchema.parse(item));
-// };
-//
-// export const getPostBySlug = async (slug: string): Promise<Article> => {
-//   const path = "/api/articles";
-//   const url = new URL(path, STRAPI_URL);
-//
-//   // Build query parameters using qs to populate cover image data
-//   url.search = qs.stringify(
-//     {
-//       filters: {
-//         slug: {
-//           $eq: slug, // Replace with the actual slug
-//         },
-//       },
-//       populate: {
-//         cover: {
-//           fields: ["url", "alternativeText"],
-//         },
-//         author: {
-//           fields: ["name", "email"],
-//           populate: {
-//             avatar: {
-//               fields: ["url", "alternativeText"],
-//             },
-//           },
-//         },
-//         category: {
-//           fields: ["name", "slug"],
-//         },
-//         tags: {
-//           fields: ["name", "slug"],
-//         },
-//       },
-//     },
-//     { encodeValuesOnly: true },
-//   );
-//
-//   // Fetch articles from Strapi
-//   const articlesData = await fetch(url.href);
-//
-//   if (!articlesData.ok) {
-//     throw new Error("Error fetching Strapi Article");
-//   }
-//
-//   const { data } = await articlesData.json();
-//
-//   const formattedArticles = data.map((item: any) => articleSchema.parse(item));
-//
-//   return formattedArticles[0];
-// };
-//
 
 export const getAboutPage = async () => {
   const path = "/api/about";
