e6839955d439979333d33df2ed2c78f1abb14970
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2025-03-24 12:49:52 +0000

    Fix main page and rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,21 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import { getAllPosts, getAllProjects } from "../lib/strapi";
 
 export async function GET(context) {
-  const posts = await getCollection("blog");
-  const projects = await getCollection("projects");
+  const posts = await getAllPosts();
+  const projects = await getAllProjects();
+
   const allPosts = posts.map((post) => ({
     ...post.data,
     link: `/blog/${post.slug}/`,
   }));
+
   const allProjects = projects.map((project) => ({
     ...project.data,
     link: `/projects/${project.slug}/`,
   }));
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
